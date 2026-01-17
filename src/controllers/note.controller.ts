a1ad13a104b9226dc142c9ee1bd2ba7f83a5c5af
Kenneth <obengmirekukenneth@gmail.com>
2026-01-17 00:46:14 +0000

    feat: update grammar checking logic to process note text before sending to LanguageTool

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import axios from "axios";
+import { remark } from "remark";
+import strip from "strip-markdown"
 
 export const checkGrammar = async (
   req: Request,
@@ -10,10 +12,13 @@ export const checkGrammar = async (
     if (!note) {
       return res.status(400).json({ status: "fail", message: "Provide note" });
     }
+    const plainText = String(
+  await remark().use(strip).process(note)
+);
     const response = await axios.post(
       "https://api.languagetool.org/v2/check",
       new URLSearchParams({
-        text: note,
+        text: plainText,
         language: "en-US",
       })
     );
@@ -28,3 +33,4 @@ export const checkGrammar = async (
       .json({ status: "fail", message: "Internal server error" });
   }
 };
+
