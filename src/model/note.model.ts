20be175585610bf54ccffa93aecd132703494b53
Kenneth <obengmirekukenneth@gmail.com>
2026-01-17 01:39:49 +0000

    added user schema and referenced user in note schema

--- src/model/note.model.ts
+++ src/model/note.model.ts
@@ -1,7 +1,12 @@
-import mongoose, { InferSchemaType, model } from "mongoose";
+import mongoose, { InferSchemaType, model, Schema } from "mongoose";
 
 const NoteSchema = new mongoose.Schema({
   content: { type: String, required: [true, "Please provide note content"] },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "user field is required"],
+  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date },
 });
@@ -12,6 +17,6 @@ NoteSchema.pre("save", async function () {
 
 type INoteSchema = InferSchemaType<typeof NoteSchema> & Document;
 
-const Note = model<INoteSchema>("Note");
+const Note = model<INoteSchema>("Note", NoteSchema);
 
 export default Note;
---
+++ src/model/user.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { InferSchemaType, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import validator from "validator";
+
+export interface IUserMethods {
+  signToken(): string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Name is required"] },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    min: 8,
+    required: [true, "Password is required"],
+    select: false,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+UserSchema.pre("save", async function (this: any) {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+});
+
+UserSchema.methods.signToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET!, {
+    expiresIn: process.env.JWT_EXPIRES_IN!,
+  } as jwt.SignOptions);
+};
+
+UserSchema.methods.comparePassword = async function (
+  this: IUser,
+  candidatePassword: string
+) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export type IUser = InferSchemaType<typeof UserSchema> &
+  IUserMethods &
+  Document;
+
+const User = model<IUser>("User", UserSchema);
+
+export default User;
