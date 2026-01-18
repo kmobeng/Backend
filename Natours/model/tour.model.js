5af3327108d0dff65258c7a471eb7faf587513a1
Kenneth <obengmirekukenneth@gmail.com>
2026-01-18 15:37:50 +0000

    model folder and files

---
+++ Natours/model/tour.model.js
@@ -0,0 +1,127 @@
+const mongoose = require("mongoose");
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+      maxlength: [
+        40,
+        "A tour name must be less than or equal to 40 characters",
+      ],
+      minlength: [
+        10,
+        "A tour name must be greater than or equal to 10 characters",
+      ],
+    },
+    slug: String,
+    duration: { type: Number, required: [true, "A tour must have a duration"] },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult",
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1"],
+      max: [5, "Rating must be below 5"],
+    },
+    ratingsQuantity: { type: Number, default: 0 },
+    price: { type: Number, required: [true, "A tour must have a price"] },
+    priceDiscout: {
+      type: Number,
+      validate: function (val) {
+        return val < this.price;
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "a tour must have a description"],
+    },
+    description: { type: String, trim: true },
+    imageCover: {
+      type: String,
+      required: [true, "a tour must have a cover image"],
+    },
+    images: [String],
+    createdAt: { type: Date, default: Date.now(), select: false },
+    startDates: [Date],
+    secretTour: { type: Boolean, default: false },
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: "Point",
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
+});
+
+tourSchema.virtual("reviews", {
+  ref: "Review",
+  foreignField: "tour",
+  localField: "_id",
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+  next();
+});
+
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+module.exports = Tour;
