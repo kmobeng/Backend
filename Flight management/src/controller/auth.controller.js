5f74e92d840c1a9932e3631e49ac5037aa110309
Kenneth <obengmirekukenneth@gmail.com>
2026-02-08 20:34:43 +0000

    added forgot password

--- Flight management/src/controller/auth.controller.js
+++ Flight management/src/controller/auth.controller.js
@@ -128,3 +128,34 @@ exports.restrictTo = () => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError("There is no user with this email", 404));
+  }
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email`;
+  try {
+    await Email.sendEmail({
+      email: user.email,
+      subject: "Your password reset token(valid for 10 mins)",
+      message,
+    });
+
+    res.status(200).json({ status: "success", message: "Token sent to email" });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new AppError("Error while sending email", 500));
+  }
+});
--- Flight management/src/routers/user.route.js
+++ Flight management/src/routers/user.route.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-const { signup, login } = require("../controller/auth.controller");
+const {
+  signup,
+  login,
+  forgotPassword,
+} = require("../controller/auth.controller");
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
 
 module.exports = router;
